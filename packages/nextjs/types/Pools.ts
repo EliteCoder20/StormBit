b7448acedfd19d1b9ac0d21f5c71767914ccaa7f
elitecoder20 <elitecoder43@gmail.com>
2024-04-20 19:27:54 +0530

    create data.ts to store data and small bug fixes

---
+++ packages/nextjs/types/Pools.ts
@@ -0,0 +1,10 @@
+export type Pools = {
+    address: string;
+    name: string;
+    marketSize: string;
+    suppliedAPY: string;
+    borrowedAPY: string;
+    totalSupplied: string;
+    totalBorrowed: string;
+  };
+  
